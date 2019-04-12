a6615291bd05c48eb1e8a7cdf5a63df1c894776e
HowarXie1995 <34116204@qq.com>
2019-04-12 11:00:52 +0800

    Upper

---
+++ ModeBus/main.cpp
@@ -0,0 +1,11 @@
+#include "datawindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DataWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ ModeBus/modbushand.h
@@ -0,0 +1,30 @@
+#ifndef MODBUSHAND_H
+#define MODBUSHAND_H
+
+#include <iostream>
+#include <cstring>
+#include <QString>
+#include <QByteArray>
+#include <QDebug>
+
+
+class ModbusHand
+{
+public://构造、析构函数
+    ModbusHand();
+    //
+
+public://成员函数
+//================================================================================================================================
+/*
+ *函数名称: transDataToMod
+ *函数功能: 把接收的字符串信号转换为modbus信号
+ *函数参数: recvStr:从text上接收的数据
+ *函数返回值: 处理成modbus格式后的数据
+*/
+    std::string transDataToMod(QString recvStr);
+private://成员函数
+
+};
+
+#endif // MODBUSHAND_H
